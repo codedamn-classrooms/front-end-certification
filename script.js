267b2501521241576082ecbe62f26cf8a6b4ed2a
Mehul Mohan <mehulmpt@gmail.com>
2022-05-21 15:06:32 +0530

    initial files

---
+++ script.js
@@ -0,0 +1,33 @@
+// you JavaScript magic goes here
+
+// Onclick on #set-timer-btn the timer should be set to the input values
+
+// input should be taken from #hours-input & #minutes-input & #seconds-input
+
+// the values should be set at #hours-counter & #minutes-counter & #seconds-counter
+
+// Onclick on #start-timer-btn the timer should start counting
+// Onclick on #stop-timer-btn the timer should stop counting
+
+const hoursCounter = document.getElementById('hours-counter')
+const minutesCounter = document.getElementById('minutes-counter')
+const secondsCounter = document.getElementById('seconds-counter')
+
+const hoursInput = document.getElementById('hours-input')
+const minutesInput = document.getElementById('minutes-input')
+const secondsInput = document.getElementById('seconds-input')
+
+function setCountdownTimer() {
+	// write the code here to set the timer values
+}
+
+function startTimer() {
+	//code to start the Timer
+}
+function stopTimer() {
+	// code to stop the timer
+}
+
+function passTests() {
+	return 'pass'
+}
