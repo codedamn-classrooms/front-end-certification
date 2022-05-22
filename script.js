79dca1e2e99e81721b93ac8bb8441d891a90acf8
Pranav <pranav.techiegeek@gmail.com>
2022-05-23 01:15:09 +0530

    changed boilerplate code

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function search() {
-  // Characters to be escaped [.*+?^${}()|[\]\\] (There characters should not be searched in the text)
+let textToSearch = document.getElementById("text-to-search");
+let paragraph = document.getElementById("paragraph");
 
-  let textToSearch = document.getElementById("text-to-search").value;
-  let paragraph = document.getElementById("paragraph");
+textToSearch.addEventListener("input", search);
+
+function search() {
+  let inputValue = textToSearch.value;
+  console.log(inputValue);
+  // Characters to be escaped [.*+?^${}()|[\]\\] (There characters should not be searched in the text)
 }
