676882414572f4979b6d4f1ba660c9ce9b97ca2c
Pranav <pranav.techiegeek@gmail.com>
2022-05-23 00:44:31 +0530

    boilerplate ready

---
+++ script.js
@@ -0,0 +1,6 @@
+function search() {
+  // Characters to be escaped [.*+?^${}()|[\]\\] (There characters should not be searched in the text)
+
+  let textToSearch = document.getElementById("text-to-search").value;
+  let paragraph = document.getElementById("paragraph");
+}
