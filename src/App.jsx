9a9c292af36a1a3c0fea83c301a4c36f9f532673
Pranav <pranav.techiegeek@gmail.com>
2022-05-22 11:56:28 +0530

    basic reeady

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>Slot Booking App</h2>
-
-      <form>
-        <input type="date"></input>
-        <div>
-          <label for="time-from">From : </label>
-          <input id="time-from" type="time"></input>
-        </div>
-        <div>
-          <label for="time-to">To : </label>
-          <input id="time-to" type="time"></input>
-        </div>
-        <button>Book the slot</button>
-      </form>
-
-      <hr />
-
-      <h2>List of Booked Slots</h2>
+      <h2>Weather API</h2>
     </div>
   );
 }
