0a6c5c32486940a9cd440a39d489e71f6e73ef9b
Pranav <pranav.techiegeek@gmail.com>
2022-05-22 12:44:02 +0530

    lab ready

---
+++ script.js
@@ -0,0 +1,6 @@
+let jsonDiv = document.getElementById("meal-json");
+let randomMealButton = document.getElementById("get-random");
+let mealCategorySelection = document.getElementById("filter-meals");
+let mealByLetter = document.getElementById("meal-by-letter");
+
+// Create 3 addEventListeners for three inputs and "on input change" the API should be called with the new value.
